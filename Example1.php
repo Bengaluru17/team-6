5b7a284641a2e044762c97c164369969d78d38fa
SHASHI PRAKASH <you@example.com>
2017-07-08 16:25:42 +0530

    example1

---
+++ Example1.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>AMOGH</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <h1></h1>
+        <div class="container">
+          <div class="jumbotron">
+            <div class="center">
+              <p>Practice it</p>
+            </div>
+         </div>
+         <div class="row">
+          <div class="col-xs-6">
+            <div class="box">
+              <p>Name of school:Delhi public school</p>
+              <p>Name of student:Rihav kumar</p>
+              <p>class:3 section:b</p>
+              <p>Blood group: O+ve</p>
+              <p>Fathers name:Rajiv sharma</p>
+              <p>Address:#621,21st cross,kumarswamylayout,bangalore 560078.</p>
+            </div>
+          </div>
+            <div class="col-xs-6">
+              <div class="box">
+                <form>
+                  <div class="form-group">
+                    <label for="School">Name of School</label>
+                    <input type="text" class="form-control" placeholder="name of school">
+                  </div>
+                  <div class="form-group">
+                    <label for="Student">Name of student</label>
+                    <input type="text" class="form-control" placeholder="Name of Student">
+                  </div>
+                  <div class="form-group">
+                    <label for="Class">Class</label>
+                    <input type="text" class="form-control" placeholder="Class">
+                  </div>
+                  <div class="form-group">
+                    <label for="Section">Section</label>
+                    <input type="text" class="form-control" placeholder="Section">
+                  </div>
+                  <div class="form-group">
+                    <label for="Blood group">Blood group</label>
+                    <input type="text" class="form-control" placeholder="Blood group">
+                  </div>
+                  <div class="form-group">
+                    <label for="Father's name">Father's Name</label>
+                    <input type="text" class="form-control" placeholder="Name of Student">
+                  </div>
+                  <div class="form-group">
+                    <label for="Address">Father's Name</label>
+                    <input type="text" class="form-control" placeholder="Address">
+                  </div>
+                  <button type="button" name="button">Submit</button>
+                </form>
+            </div>
+          </div>
+         </div>
+        </div>
+        <?php
+        ?>
+    </body>
+</html>
