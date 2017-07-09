75c5948bf068139db824f1ad7c57dc8f4a953f1f
SHASHI PRAKASH <shashiprakash.srk@gmail.com>
2017-07-09 10:39:16 +0530

    Add files via upload

--- buttons.php
+++ buttons.php
@@ -36,51 +36,80 @@
                 <form method="post">
                   <div class="form-group">
                     <label for="applicant">Name of the applicant</label>
-                    <input type="text" class="form-control" placeholder="Applicant name">
+                    <input id="nm" type="text" class="form-control" placeholder="Applicant name">
                   </div>
 				              <div class="form-group">
                     <label for="Gender">Gender</label>
                     <form action=""><br>
-                      <input type="radio" id="gender" name="gender" value="1"> Male<br>
-                      <input type="radio" id="gender" name="gender" value="0"> Female<br>
-                      <input type="radio" id="gender" name="gender" value="-1"> Other<br>
+                      <input type="radio" id="gender" name="gender" value="Male"> Male<br>
+                      <input type="radio" id="gender" name="gender" value="Female"> Female<br>
+                      <input type="radio" id="gender" name="gender" value="Other"> Other<br>
                       
                     </form>
                   <div class="form-group">
                     <label for="Card">Type of card</label>
                     <form action="">
-                      <input type="radio" name="card" value="Credit"> Credit<br>
-                      <input type="radio" name="card" value="Debit"> Debit<br>
+                      <input id="crd" type="radio" name="card" value="Credit">Credit<br>
+                      <input id="crd" type="radio" name="card" value="Debit">Debit<br>
 
                     </form>
                   </div>
                   <div class="form-group">
                     <label for="Account">Account type</label>
                     <form action="">
-                      <input type="radio" name="gender" value="savings">Savings<br>
-                      <input type="radio" name="gender" value="current">Current<br>
+                      <input id="actyp" type="radio" name="actype" value="savings">Savings<br>
+                      <input id="actyp" type="radio" name="actype" value="current">Current<br>
 
                     </form>
                   </div>
                  <div class="form-group">
                     <label for="Services">Did you like our services?</label>
                     <form action="myfunc">
-                      <input type="radio" name="type" value="Yes">Yes<br>
-                      <input type="radio" name="type" value="No">No<br>
+                      <input id="lk" type="radio" name="type" value="Yes">Yes<br>
+                      <input id="lk" type="radio" name="type" value="No">No<br>
                     </form>
                   <div class="form-group">
                     <label for="Gender">Recommend us</label>
                     <form action=""><br>
-                      <input type="radio" name="gender" value="Yes">Yes<br>
-                      <input type="radio" name="gender" value="No">No<br>
+                      <input id="rc" type="radio" name="rec" value="Yes">Yes<br>
+                      <input id="rc "type="radio" name="rec" value="No">No<br>
                     </form>
                   </div>
 
-                  <button type="button" name="button" onclick="myfunc">Submit</button>
+                  <button type="button" name="button" onclick="myfunc()">Submit</button>
                 </form>
             </div>
           </div>
          </div>
         </div>
+		<script type="text/javascript">  
+		function myfunc(){
+			
+		var flag=0;
+		var nm,gen,crd,actyp,lk,rc;
+		
+		
+		
+		crd=document.querySelector('input[name="card"]:checked').value;
+		nm=document.getElementById("nm").value;
+		gen=document.querySelector('input[name="gender"]:checked').value;
+		actyp=document.querySelector('input[name="actype"]:checked').value;
+		lk=document.querySelector('input[name="type"]:checked').value;
+		rc=document.querySelector('input[name="rec"]:checked').value;
+		console.log(gen,crd,actyp,lk,rc);
+		
+		if(!(nm=="Aanya B"))flag=1;
+		if(!(gen=="Male"))flag=1;
+		if(!(crd=="Debit"))flag=1;
+		if(!(actyp=="Savings"))flag=1;
+		if(!(rc=="Yes"))flag=1;
+		if(!(lk=="Yes"))flag=1;
+		
+		if(flag) alert('You entered wrong Info');
+		else alert("Great Job!");
+		
+		}
+		
+		</script>
     </body>
 </html>
